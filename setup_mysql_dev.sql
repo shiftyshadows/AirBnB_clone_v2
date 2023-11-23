f458ce71e00b9afb5ab5bc2d90889bcdab1f0dc8
Shiftyshadows <purple.quasar@proton.me>
2023-11-23 18:11:44 +0300

    3. MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -8,4 +8,13 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Grant SELECT privilege on performance_schema to hbnb_dev
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Revoke any additional privileges on performance_schema
+REVOKE ALL PRIVILEGES ON performance_schema.* FROM 'hbnb_dev'@'localhost';
+
+-- Grant usage privilege on performance_schema to hbnb_dev
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
