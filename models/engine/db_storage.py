7523a7c178beae90660f85f8a204097b4dec0071
Shiftyshadows <purple.quasar@proton.me>
2023-12-27 05:59:52 +0300

    Updated to pycodestyle.

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -8,12 +8,8 @@ from models.base_model import BaseModel
 from models.state import State
 from models.city import City
 
-Base=declarative_base()
-#os.environ["HBNB_MYSQL_USER"] = "hbnb_dev"
-#os.environ["HBNB_MYSQL_PWD"] = "hbnb_dev_pwd"
-#os.environ["HBNB_MYSQL_HOST"] = "localhost"
-#os.environ["HBNB_MYSQL_DB"] = "hbnb_dev_db"
-#os.environ["HBNB_TYPE_STORAGE"] = "db"
+Base = declarative_base()
+
 
 class DBStorage:
     """
@@ -38,18 +34,20 @@ class DBStorage:
         ppp = True
         env = os.environ.get('HBNB_ENV')
 
-        connection_string = "{}+{}://{}:{}@{}/{}".format(dialect, driver, user, password, host, database)
+        connection_string = "{}+{}://{}:{}@{}/{}".format(
+            dialect, driver, user, password, host, database)
         self.__engine = create_engine(connection_string, pool_pre_ping=ppp)
         if env == 'test':
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """Query on the current database session (self.__session) all objects
-        depending on the class name (argument cls).
+        """Query on the current database session (self.__session)
+           all objects depending on the class name (argument cls).
 
-        If cls is None, query all types of objects (User, State, City, Amenity, Place, and Review).
+           If cls is None, query all types of objects.
+           (User, State, City, Amenity, Place, and Review).
 
-        Returns:
+           Returns:
             dict: Dictionary with key-value pairs where
                 key = <class-name>.<object-id>
                 value = object
@@ -64,7 +62,6 @@ class DBStorage:
         for model_class in classes_to_query:
             model_name = model_class.__name__
             query_result = self.__session.query(model_class).all()
-
             for obj in query_result:
                 key = f"{model_name}.{obj.id}"
                 result[key] = obj
@@ -74,7 +71,8 @@ class DBStorage:
     def get_session(self):
         """ This method returns a session using the engine."""
         if self.__session is None:
-            self.__session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+            self.__session = sessionmaker(
+                bind=self.__engine, expire_on_commit=False)
         return self.__session()
 
     def new(self, obj):
@@ -92,17 +90,19 @@ class DBStorage:
         finally:
             self.__session.close()
 
-
     def delete(self, obj=None):
         """Delete obj from the current database session if not None."""
         if obj:
             self.__session.delete(obj)
 
     def reload(self):
-        """Create all tables in the database and create the current database session."""
+        """
+           Create all tables in the database and create the current
+           database session.
+        """
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        self.__session = scoped_session(session_factory)
+        sesh_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        self.__session = scoped_session(sesh_factory)
 
     def close(self):
         """Close the current database session."""
