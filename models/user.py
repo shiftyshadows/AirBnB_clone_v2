edf2a3e8937d7569f8734497f99ad2bb43234ef5
Shiftyshadows <purple.quasar@proton.me>
2023-12-27 06:12:29 +0300

    7. DBStorage - User

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ This module defines the User class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, create_engine
 
 
 class User(BaseModel, Base):
@@ -15,3 +15,7 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+engine = create_engine(
+    "mysql+mysqldb://hbnb_dev:hbnb_dev_pwd@localhost/hbnb_dev_db", echo=False)
+Base.metadata.create_all(engine)
