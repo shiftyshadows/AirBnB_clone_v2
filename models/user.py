94b16998f0e3c0b801a0412b7cee1a87a69a9d09
Shiftyshadows <purple.quasar@proton.me>
2023-12-27 06:10:27 +0300

    7. DBStorage - User

--- models/user.py
+++ models/user.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """ This module defines the User class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """ User class that inherits from BaseModel """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
