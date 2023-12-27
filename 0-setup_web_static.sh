51ce3f1bb7fa7472008b386cbd436a804d509913
Shiftyshadows <purple.quasar@proton.me>
2023-12-28 02:49:58 +0300

    0. Prepare your web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+#Script that sets up your web servers for the deployment of web_static
+# Install Nginx if not already installed
+if ! command -v nginx &> /dev/null
+then
+    # Update source list
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] https://nginx.org/packages/ubuntu/ focal nginx" | sudo tee /etc/apt/sources.list.d/nginx.list
+    echo "deb-src [arch=amd64 signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] https://nginx.org/packages/ubuntu/ focal nginx" | sudo tee -a /etc/apt/sources.list.d/nginx.list
+    # Recieve signing keys
+    sudo gpg --keyserver keyserver.ubuntu.com --recv-keys ABF5BD827BD9BF62
+    sudo gpg --export --armor ABF5BD827BD9BF62 | sudo gpg --dearmor -o /usr/share/keyrings/nginx-archive-keyring.gpg
+    # Update package list
+    sudo apt-get update
+    # Install nginx
+    sudo apt-get install -y nginx
+fi
+
+#Create folders
+sudo mkdir -p /data/web_static/releases/test
+sudo cp index.html /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/current/
+
+# Create symbolic link
+symbolic_link="/data/web_static/current"
+target_folder="/data/web_static/releases/test/"
+sudo ln -sf "$target_folder" "$symbolic_link"
+
+#Give ownership of the /data/ folder to the ubuntu user AND group
+sudo chown -R ubuntu:ubuntu /data/
+
+#Update the Nginx configuration
+nginx_config="/etc/nginx/sites-available/default"
+symbolic_link="/data/web_static/current"
+echo "
+server {
+    listen 80;
+    server_name _;
+
+    location /hbnb_static {
+        alias $symbolic_link/;
+        index index.html index.htm;
+    }
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+}" | sudo tee "$nginx_config" > /dev/null
+
+# Restart Nginx
+sudo service nginx restart
--- create_table.sql
+++
@@ -1,7 +0,0 @@
--- Creates states table
-CREATE TABLE states (
-    id VARCHAR(36) PRIMARY KEY,
-    name VARCHAR(255),
-    created_at DATETIME,
-    updated_at DATETIME
-);
