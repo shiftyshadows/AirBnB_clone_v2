5520241edc7370d6e8f3ab83181a236f0500a0f7
Shiftyshadows <purple.quasar@proton.me>
2024-01-04 04:30:58 +0300

    0. Hello Flask!

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+# Route definition with strict_slashes=False
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    # Run the application on 0.0.0.0, port 5000
+    app.run(host='0.0.0.0', port=5000)
+
