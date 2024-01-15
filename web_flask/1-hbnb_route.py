ef8dff010a460d3a0044c338537f351b30154d37
Shiftyshadows <purple.quasar@proton.me>
2024-01-15 22:39:26 +0300

    1. HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" This module defines a script that starts a Flask web application."""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    # Run the Flask app on 0.0.0.0, port 5000
+    app.run(host='0.0.0.0', port=5000)
