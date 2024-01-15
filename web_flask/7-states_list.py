340db78c50efc2ae542e33542be0c66f0d2b9d67
Shiftyshadows <purple.quasar@proton.me>
2024-01-15 23:16:07 +0300

    8. List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-'''A simple Flask web application.
-'''
+""" This module defines a script that starts a Flask web application."""
 from flask import Flask, render_template
-
 from models import storage
 from models.state import State
 
