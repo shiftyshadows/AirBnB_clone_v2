b5b9b6502980fd99b9da3872340bca83d1170e60
Shiftyshadows <purple.quasar@proton.me>
2024-01-15 23:14:49 +0300

    8. List of states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""
-   A simple Flask web application.
-"""
-from flask import Flask, render_template
+""" This module defines a script that starts a Flask web application."""
 
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 
