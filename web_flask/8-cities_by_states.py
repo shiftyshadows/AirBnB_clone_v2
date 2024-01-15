b155a7a2fa9430975cc637125846e2b785c613f6
Shiftyshadows <purple.quasar@proton.me>
2024-01-16 00:15:08 +0300

    Revert "8. List of states"
    This reverts commit a16521d02aba1562bbc93ce29aa383fd6412bf92.

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" This module defines a script that starts a Flask web application."""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/cities_by_states')
+def cities_by_states():
+    all_states = list(storage.all(State).values())
+    all_states.sort(key=lambda x: x.name)
+    for state in all_states:
+        state.cities.sort(key=lambda x: x.name)
+    ctxt = {
+        'states': all_states
+    }
+    return render_template('8-cities_by_states.html', **ctxt)
+
+
+@app.teardown_appcontext
+def flask_teardown(exc):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
