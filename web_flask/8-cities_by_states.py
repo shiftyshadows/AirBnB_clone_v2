9e2dd2420be2180bbe2d8a56f75ac44d96aa42ce
Shiftyshadows <purple.quasar@proton.me>
2024-01-15 23:10:31 +0300

    9. Cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 '''A simple Flask web application.
 '''
-from flask import Flask, render_template, Markup
+from flask import Flask, render_template
 
 from models import storage
-from models.amenity import Amenity
-from models.place import Place
 from models.state import State
 
 
@@ -14,25 +12,17 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/hbnb')
-def hbnb():
-    '''The hbnb page.'''
+@app.route('/cities_by_states')
+def cities_by_states():
+    '''The cities_by_states page.'''
     all_states = list(storage.all(State).values())
-    amenities = list(storage.all(Amenity).values())
-    places = list(storage.all(Place).values())
     all_states.sort(key=lambda x: x.name)
-    amenities.sort(key=lambda x: x.name)
-    places.sort(key=lambda x: x.name)
     for state in all_states:
         state.cities.sort(key=lambda x: x.name)
-    for place in places:
-        place.description = Markup(place.description)
     ctxt = {
-        'states': all_states,
-        'amenities': amenities,
-        'places': places
+        'states': all_states
     }
-    return render_template('100-hbnb.html', **ctxt)
+    return render_template('8-cities_by_states.html', **ctxt)
 
 
 @app.teardown_appcontext
