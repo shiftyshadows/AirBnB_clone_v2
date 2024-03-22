00c7d32ef272dfaf4f9c94b7c9e6b7f9749ea989
Shiftyshadows <purple.quasar@proton.me>
2024-03-22 18:42:00 +0300

    5. Puppet for setup

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -27,65 +27,65 @@ $nginx_conf = "server {
 package { 'nginx':
   ensure   => 'present',
   provider => 'apt'
-} ->
+}
 
-file { '/data':
+-> file { '/data':
   ensure  => 'directory'
-} ->
+}
 
-file { '/data/web_static':
+-> file { '/data/web_static':
   ensure => 'directory'
-} ->
+}
 
-file { '/data/web_static/releases':
+-> file { '/data/web_static/releases':
   ensure => 'directory'
-} ->
+}
 
-file { '/data/web_static/releases/test':
+-> file { '/data/web_static/releases/test':
   ensure => 'directory'
-} ->
+}
 
-file { '/data/web_static/shared':
+-> file { '/data/web_static/shared':
   ensure => 'directory'
-} ->
+}
 
-file { '/data/web_static/releases/test/index.html':
+-> file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
   content => "Holberton School Puppet\n"
-} ->
+}
 
-file { '/data/web_static/current':
+-> file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test'
-} ->
+}
 
-exec { 'chown -R ubuntu:ubuntu /data/':
+-> exec { 'chown -R ubuntu:ubuntu /data/':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
 file { '/var/www':
   ensure => 'directory'
-} ->
+}
 
-file { '/var/www/html':
+-> file { '/var/www/html':
   ensure => 'directory'
-} ->
+}
 
-file { '/var/www/html/index.html':
+-> file { '/var/www/html/index.html':
   ensure  => 'present',
   content => "Holberton School Nginx\n"
-} ->
+}
 
-file { '/var/www/html/404.html':
+-> file { '/var/www/html/404.html':
   ensure  => 'present',
   content => "Ceci n'est pas une page\n"
-} ->
+}
 
-file { '/etc/nginx/sites-available/default':
+-> file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
   content => $nginx_conf
-} ->
+}
 
-exec { 'nginx restart':
+-> exec { 'nginx restart':
   path => '/etc/init.d/'
 }
