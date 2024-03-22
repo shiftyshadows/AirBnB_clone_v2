7f225676208e87985bcc0c5a9ea40373ab865248
Shiftyshadows <purple.quasar@proton.me>
2024-03-22 18:26:51 +0300

    5. Puppet for setup

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,23 +1,21 @@
 #!/usr/bin/puppet apply
 # AirBnB clone web server setup and configuration
-# Define class for nginx installation and configuration
-class web_server {
-    # Install nginx package
-    package { 'nginx':
-        ensure => installed,
-    }
+# Install nginx package
+package { 'nginx':
+    ensure => installed,
+}
 
-    # Define nginx service
-    service { 'nginx':
-        ensure  => running,
-        enable  => true,
-        require => Package['nginx'],
-    }
+# Define nginx service
+service { 'nginx':
+    ensure  => running,
+    enable  => true,
+    require => Package['nginx'],
+}
 
-    # Define nginx server configuration
-    file { '/etc/nginx/sites-available/default':
-        ensure  => file,
-        content => "error_log  /var/log/nginx/error.log notice;
+# Define nginx server configuration
+file { '/etc/nginx/sites-available/default':
+    ensure  => file,
+    content => "error_log  /var/log/nginx/error.log notice;
 pid        /var/run/nginx.pid;
 
 events {
@@ -53,58 +51,58 @@ http {
         }
     }
 }",
-        require => Package['nginx'],
-        notify  => Service['nginx'],
-    }
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+}
 
-    # Create directories
-    $directories = [
-        '/data/web_static/releases/test',
-        '/data/web_static/shared',
-        '/var/www/html',
-        '/var/www/error',
-    ]
+# Create directories
+$directories = [
+    '/data/web_static/releases/test',
+    '/data/web_static/shared',
+    '/var/www/html',
+    '/var/www/error',
+]
 
-    # Ensure directories are present
-    ensure_resource('file', $directories, { ensure => directory })
+# Ensure directories are present
+ensure_resource('file', $directories, { ensure => directory })
 
-    # Set permissions
-    file { '/var/www':
-        ensure  => directory,
-        mode    => '0755',
-        recurse => true,
-        owner   => 'ubuntu',
-        group   => 'ubuntu',
-    }
+# Set permissions
+file { '/var/www':
+    ensure  => directory,
+    mode    => '0755',
+    recurse => true,
+    owner   => 'ubuntu',
+    group   => 'ubuntu',
+}
 
-    # Define default index.html
-    file { '/var/www/html/index.html':
-        ensure  => file,
-        content => 'Hello World!',
-        owner   => 'ubuntu',
-        group   => 'ubuntu',
-    }
+# Define default index.html
+file { '/var/www/html/index.html':
+    ensure  => file,
+    content => 'Hello World!',
+    owner   => 'ubuntu',
+    group   => 'ubuntu',
+}
 
-    # Define 404.html
-    file { '/var/www/error/404.html':
-        ensure  => file,
-        content => "Ceci n'est pas une page",
-        owner   => 'ubuntu',
-        group   => 'ubuntu',
-    }
+# Define 404.html
+file { '/var/www/error/404.html':
+    ensure  => file,
+    content => "Ceci n'est pas une page",
+    owner   => 'ubuntu',
+    group   => 'ubuntu',
+}
 
-    # Define symbolic link for static content
-    file { '/data/web_static/current':
-        ensure => link,
-        target => '/data/web_static/releases/test',
-        owner  => 'ubuntu',
-        group  => 'ubuntu',
-    }
+# Define symbolic link for static content
+file { '/data/web_static/current':
+    ensure => link,
+    target => '/data/web_static/releases/test',
+    owner  => 'ubuntu',
+    group  => 'ubuntu',
+}
 
-    # Define homepage index.html
-    file { '/data/web_static/releases/test/index.html':
-        ensure  => file,
-        content => "<!DOCTYPE html>
+# Define homepage index.html
+file { '/data/web_static/releases/test/index.html':
+    ensure  => file,
+    content => "<!DOCTYPE html>
 <html lang='en-US'>
         <head>
                 <title>Home - AirBnB Clone</title>
@@ -113,11 +111,7 @@ http {
                 <h1>Welcome to AirBnB!</h1>
         <body>
 </html>",
-        owner   => 'ubuntu',
-        group   => 'ubuntu',
-        require => File['/data/web_static/current'],
-    }
+    owner   => 'ubuntu',
+    group   => 'ubuntu',
+    require => File['/data/web_static/current'],
 }
-
-# Apply web_server class
-include web_server
