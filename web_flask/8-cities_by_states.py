ed171efa979a8793b566aeaa2a188e9bcf649ec2
Shiftyshadows <purple.quasar@proton.me>
2024-03-24 15:13:03 +0300

    9. Cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,10 +4,10 @@
 """
 from flask import Flask, render_template
 from models import storage
-#from models.state import State
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
 
 @app.teardown_appcontext
 def teardown_session(exception):
