504ec8707c11fae92d6dec092c031a6844bb9247
Shiftyshadows <purple.quasar@proton.me>
2024-09-25 05:08:20 +0300

    2. C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,64 +1,35 @@
 #!/usr/bin/python3
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
 """
-This module defines a script that starts a Flask web application.
-
-The web application listens on 0.0.0.0, port 5000, and has the following
-routes:
-
-1. `/`: Displays the message 'Hello HBNB!'.
-2. `/hbnb`: Displays the message 'HBNB'.
-3. `/c/<text>`: Displays 'C' followed by the value of the text variable.
-    - The text variable replaces underscores with spaces in the output.
-    - Example: `/c/hello_hbnb` returns `C hello hbnb`.
-
-This module uses the `escape` function from Flask to ensure that the input
-text is safely handled.
-"""
-
-from flask import Flask, escape
+from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """
-    This route returns the string 'Hello HBNB!'.
-
-    Returns:
-        str: A string 'Hello HBNB!'.
-    """
-    return 'Hello HBNB!'
+    """Displays 'Hello HBNB!'."""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """
-    This route returns the string 'HBNB'.
-
-    Returns:
-        str: A string 'HBNB'.
-    """
-    return 'HBNB'
+    """Displays 'HBNB'."""
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def custom_text(text):
-    """
-    This route returns 'C' followed by the value of the `text` variable,
-    with underscores replaced by spaces.
-
-    Args:
-        text (str): The input text from the URL, with underscores replaced
-        by spaces.
-
-    Returns:
-        str: A formatted string 'C <text>'.
-    """
-    text = escape(text).replace('_', ' ')
-    return 'C {}'.format(text)
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
-if __name__ == '__main__':
-    # Run the Flask app on 0.0.0.0, port 5000
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
