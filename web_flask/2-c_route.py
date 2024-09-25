80de0e6e580f15df59165380ed3e10f82528c8fd
Shiftyshadows <purple.quasar@proton.me>
2024-09-25 04:55:37 +0300

    2. C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" A module that starts a Flask web application. """
+""" This module defines a script that starts a Flask web application."""
 from flask import Flask, escape
 
 app = Flask(__name__)
