fda11471101ef9428d96d2b624882c984cbd5b84
Shiftyshadows <purple.quasar@proton.me>
2024-09-25 04:54:29 +0300

    2. C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,25 +6,20 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-# Route for the root path
 def hello_hbnb():
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-# Route for the /hbnb path
 def hbnb():
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-# Route for the /c/<text> path
 def custom_text(text):
-    # Replace underscore symbols with a space
     text = escape(text).replace('_', ' ')
     return 'C {}'.format(text)
 
 
 if __name__ == '__main__':
-    # Run the Flask app on 0.0.0.0, port 5000
     app.run(host='0.0.0.0', port=5000)
