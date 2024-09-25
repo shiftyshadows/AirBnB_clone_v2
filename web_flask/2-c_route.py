3021bdd6720c9375e090772c0a895e9c8398227d
Shiftyshadows <purple.quasar@proton.me>
2024-09-25 04:53:23 +0300

    2. C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,35 +1,25 @@
 #!/usr/bin/python3
-"""
-   This module defines a script that starts a Flask web application.
-"""
+""" A module that starts a Flask web application. """
 from flask import Flask, escape
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 # Route for the root path
 def hello_hbnb():
-    """
-       Method print text
-    """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 # Route for the /hbnb path
 def hbnb():
-    """
-       Method print text
-    """
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 # Route for the /c/<text> path
 def custom_text(text):
-    """
-       Method format text
-    """
     # Replace underscore symbols with a space
     text = escape(text).replace('_', ' ')
     return 'C {}'.format(text)
